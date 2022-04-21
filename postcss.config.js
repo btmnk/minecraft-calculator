cc823c9c6fae0a1df24c99bb7c643eb9a3f91443
btmnk <contact@bitmonk.de>
2022-04-21 16:09:56 +0200

    INIT

---
+++ postcss.config.js
@@ -0,0 +1,17 @@
+const postcssPresetEnv = require("postcss-preset-env");
+const postcssImport = require("postcss-import");
+
+const config = () => ({
+  plugins: [
+    postcssImport(),
+    postcssPresetEnv({
+      stage: 3,
+      features: {
+        "nesting-rules": true,
+        "custom-media-queries": true,
+      },
+    }),
+  ],
+});
+
+module.exports = config;
---
+++ src/ui/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <span>Hello world!</span>
+    </div>
+  );
+};
+
+export { App };
